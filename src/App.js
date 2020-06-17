5d2e1762b0f59e236978b1ede9145d3204515115
Cristian Wesley <cristian.correia@lanlink.com>
2020-06-17 17:12:29 -0300

    adicionando messagem e validações

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark as ColorTheme } from "react-syntax-highlighter/dist/esm/styles/hljs";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 // Chamada de componentes
@@ -9,26 +10,57 @@ import Card from "./components/Card";
 import { CardForm } from "./styles";
 
 function App() {
-  const [topRight, setTopRight] = useState("");
-  const [topLeft, setTopLeft] = useState("");
-  const [bottomRight, setBottomRight] = useState("");
-  const [bottomLeft, setBottomLeft] = useState("");
+  const [topRight, setTopRight] = useState(0);
+  const [topLeft, setTopLeft] = useState(0);
+  const [bottomRight, setBottomRight] = useState(0);
+  const [bottomLeft, setBottomLeft] = useState(0);
+
+  const result = `{
+    border-top-left-radius: ${topLeft}px;
+    border-top-right-radius: ${topRight}px;
+    border-top-left-radius: ${bottomRight}px;
+    border-bottom-left-radius: ${bottomLeft}px;
+}`;
+
+  const copyText = () => {
+    //Cria um elemento input (pode ser um textarea)
+    let inputTest = document.createElement("input");
+    inputTest.value = result;
+    //Anexa o elemento ao body
+    document.body.appendChild(inputTest);
+    //seleciona todo o texto do elemento
+    inputTest.select();
+    //executa o comando copy
+    //aqui é feito o ato de copiar para a area de trabalho com base na seleção
+    document.execCommand("copy");
+    //remove o elemento
+    document.body.removeChild(inputTest);
+
+    return toast(" ✔ Copiado com sucesso !");
+  };
+
+  const onChange = (value, callBack) => {
+    if (/^[\d|.]+$/g.test(value) || value === "") {
+      callBack(value);
+    }
+  };
 
   return (
     <div className="App">
+      <ToastContainer />
       <CardForm className="cardForm">
         <div className="row">
           <input
             className="inputPreviewerBorderRadius"
             placeholder="Top-Left"
             value={topLeft}
-            onChange={(e) => setTopLeft(e.target.value)}
+            onChange={(e) => onChange(e.target.value, setTopLeft)}
           />
           <input
             className="inputPreviewerBorderRadius"
             placeholder="Top-Right"
             value={topRight}
-            onChange={(e) => setTopRight(e.target.value)}
+            onChange={(e) => onChange(e.target.value, setTopRight)}
           />
         </div>
         <div className="card">
@@ -39,12 +71,7 @@ function App() {
             topLeft={topLeft}
           >
             <SyntaxHighlighter language="css" style={ColorTheme}>
-              {`{
-  border-top-left-radius: ${topLeft}px;
-  border-top-right-radius: ${topRight}px;
-  border-bottom-right-radius: ${bottomRight}px;
-  border-bottom-left-radius: ${bottomLeft}px;
-}`}
+              {result}
             </SyntaxHighlighter>
           </Card>
         </div>
@@ -54,13 +81,16 @@ function App() {
             className="inputPreviewerBorderRadius"
             placeholder="Bot-Left"
             value={bottomLeft}
-            onChange={(e) => setBottomLeft(e.target.value)}
+            onChange={(e) => onChange(e.target.value, setBottomLeft)}
           />
+          <button className="button" onClick={copyText}>
+            Copiar
+          </button>
           <input
             className="inputPreviewerBorderRadius"
             placeholder="Bot-Right"
             value={bottomRight}
-            onChange={(e) => setBottomRight(e.target.value)}
+            onChange={(e) => onChange(e.target.value, setBottomRight)}
           />
         </div>
       </CardForm>
